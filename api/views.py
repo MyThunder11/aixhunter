8ab4fcb4dc73273a17c83f3c4b47ed0abe7be55e
Cyril Laferriere <cyrillaf@gmail.com>
2023-06-08 00:10:48 +0200

    added file formats and error handling

--- api/views.py
+++ api/views.py
@@ -12,26 +12,34 @@ import requests
 
 
 
-
 class Prediction(APIView):
     def post(self, request):
-        data = json.loads(request.body)
-        print(type(data['image']))
-        base64_image = data['image']  # 'image' is the key in the JSON object
-        if base64_image is None:
-            return Response({'error': 'No image found'}, status=400)
-        try:
-            # base64_image = base64_image.split(',')[1]
-            imgdata = base64.b64decode(base64_image)
-            image = Image.open(io.BytesIO(imgdata))
-            image.save('test.jpeg')
-            path = 'test.jpeg'
-            score = pred(ApiConfig.model , path)
-            prediction = 1 if score >= 0.99 else 0
-            response_dict = {"Prediction": prediction, "Score": score}
-        except Exception as e:
-            return Response({'error': str(e)}, status=400)
-
+        if 'file' in request.FILES:
+            image = request.FILES['file']
+            try:
+                file_extension = os.path.splitext(image.name)[1]
+                path = f'temp{file_extension}'
+                with open(path, 'wb') as saved_file:
+                    saved_file.write(image.read())
+                    print('image saved locally')
+            except Exception as e:
+                return Response({'error': str(e)}, status=400)
+        else:
+            base64_image = request.data['image']  # 'image' is the key in the JSON object
+            if base64_image is None:
+                return Response({'error': 'No image found'}, status=400)
+            try:
+                # base64_image = base64_image.split(',')[1] # a rétintégrer en fonction du format reçu
+                imgdata = base64.b64decode(base64_image)
+                image = Image.open(io.BytesIO(imgdata))
+                file_extension = image.format.lower()
+                path = f'temp.{file_extension}'
+                image.save(path)
+            except Exception as e:
+                return Response({'error': str(e)}, status=400)
+        score = pred(ApiConfig.model , path)
+        prediction = 1 if score >= 0.99 else 0
+        response_dict = {"Prediction": prediction, "Score": score}
         return Response(response_dict, status=200)
 
     def get(self, request):
