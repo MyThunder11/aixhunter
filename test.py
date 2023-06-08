73ef1aae85256f5d6ca6cbd0f645a4b2b51898fd
Amine Benani <aminebenani44@gmail.com>
2023-06-08 10:37:08 +0200

    tested test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from io import BytesIO
 
 url = 'http://127.0.0.1:8000/'
 
-params = {'url': 'https://cdn.wamda.com/feature-images/53ce9644a32beb0.png',
+params = {'url': 'https://static01.nyt.com/newsgraphics/2020/11/12/fake-people/4b806cf591a8a76adfc88d19e90c8c634345bf3d/fallbacks/mobile-06.jpg',
           'method': 'url'}
 
 params_2 = {'method': 'b64',
@@ -18,6 +18,6 @@ params_2 = {'method': 'b64',
 
 b64_img = '/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL/2wBDAQkJCQwLDBgNDRgyIRwhMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjL/wAARCAAgACADASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwDhQop+2kztUkgnHpVYXVxJdrCtoyR9WkkYdPYCu25w2Le2kK06ii4iPkKTioo5XM+xl+XHBxU69KXvSsMUCgilzQaAP//Z'
 payload ={"filename": "photo.jpeg", "filedata": b64_img}
-resp = requests.get(url=url, data=payload, params=params_2)
+resp = requests.get(url=url, params=params)
 
-print(resp.content)
+print(resp.url)
