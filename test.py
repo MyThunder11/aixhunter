cfd8f96ca584206be30a8eeb239e74a2d714a60b
Cyril Laferriere <cyrillaf@gmail.com>
2023-06-08 11:08:18 +0200

    api post

--- test.py
+++ test.py
@@ -10,27 +10,40 @@ def post_b64_test():
     with open("szAKSQPN.jpg", "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
 
-    """ with open('base64.txt', 'r') as image_text:
-        encoded_string = image_text.read() """
+    encoded_string_2= "data:image/png;base64, " + encoded_string
 
     # Specify the url of your Django server
     url = "http://localhost:8000"
 
     # Format the data as JSON
-    data = json.dumps({"image": encoded_string})
+    data = json.dumps({"url": encoded_string})
+    data2 = json.dumps({'url': encoded_string_2})
 
     # Send a POST request to the server
-    response = requests.post(url, data=data, headers={'Content-Type': 'application/json'})
+    response = requests.post(url, data=data,  headers={'Content-Type': 'application/json'})
+    response2 = requests.post(url, data=data2, headers={'Content-Type': 'application/json'})
 
     # Print out the response from the server
-    return response.content
+    return response.content, response.status_code #, response2.content, response2.status_code
 
 def post_img_test():
     # define URL
     url = "http://localhost:8000"
 
     # Send a POST request to the server
-    response = requests.post(url, files={'file':open("szAKSQPN.jpg", "rb")})
+    response = requests.post(url, files={'file': open("szAKSQPN.jpg", "rb")})
+
+    # Print out the response from the server
+    return response.content
+
+def post_url():
+    # define URL
+    url = "http://localhost:8000"
+
+    scrape_url = 'https://miro.medium.com/v2/resize:fit:1400/0*EAwg7WIIMhgnSfLf.png'
+
+    # Send a POST request to the server
+    response = requests.post(url, data={'url':scrape_url})
 
     # Print out the response from the server
     return response.content
@@ -52,10 +65,11 @@ def get_url():
 def test_forbidden():
     scrap_url = 'https://webtoon-phinf.pstatic.net/20230215_68/1676406944721ykbfR_JPEG/6HelloBaby_thumbnail_desktop.jpg?type=a210'
     url = "http://localhost:8000"
-    response = requests.get(url, params={'url': scrap_url})
+    response = requests.post(url,  data={'url': scrap_url})
     return response.content, response.status_code
 
 print(post_img_test())
 print(post_b64_test())
+print(post_url())
 print(get_url())
 print(test_forbidden())
